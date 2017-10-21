eaef4f3ad3baf6ca9e85be8a8167cb664c47bbc5
Christian Brüggemann <mail@cbruegg.com>
2017-10-21 13:46:17 +0200

    Update start script

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ DIR=/usr/local/java_apps/mensaupbservice
 
 cd $DIR
 ./gradlew shadowJar
-sudo -u www-data nohup "/usr/bin/java -jar build/libs/mensaupbservice-1.0-all.jar" &> $DIR/server.log &
+sudo -u www-data nohup java -jar build/libs/mensaupbservice-1.0-all.jar &> $DIR/server.log &
 echo $! > $DIR/server.pid
